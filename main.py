6fdee099ea6433b9111a6fcc04133021baa4d431
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-13 13:02:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ app = pyrogram.Client(
        bot_token=Config.BOT_TOKEN,
        api_id=Config.APP_ID,
        api_hash=Config.API_HASH,
+       plugins=dict(root="siri")
     )
 
 @Client.on_message(filters.command('start') & filters.private)
