776b8cf879af0a466d3c7b116ed61e9eed759af8
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-23 18:56:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegraph import upload_file
 from config import Config
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-Jebot = Client(
+Peaky = Client(
    "Telegraph Uploader",
    api_id=Config.APP_ID,
    api_hash=Config.API_HASH,
