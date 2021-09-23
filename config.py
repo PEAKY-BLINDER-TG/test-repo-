dba236ca2c1ebec1cc9da0285252d0f9efedd92f
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-23 15:13:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 import os
-import re
-from youtube_dl import YoutubeDL
+import os
 
-class Config:
-    APP_ID = int(os.environ.get("APP_ID", ''))
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+
     API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name with /song command")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
-    OWNER = os.environ.get("OWNER", "shamilhabeeb") 
-    DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
-    msg = {}
