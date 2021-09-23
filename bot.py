6648cbddca0204efd2cca76b3c7b23656d7291c4
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-23 15:37:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,53 +77,6 @@ async def about(client, message):
             disable_web_page_preview=True,        
             parse_mode="html")
 
-@Client.on_message(filters.photo)
-async def telegraphphoto(client, message):
-    msg = await message.reply_text("Uploading To Telegraph...")
-    download_location = await client.download_media(
-        message=message, file_name='root/jetg')
-    try:
-        response = upload_file(download_location)
-    except:
-        await msg.edit_text("Photo size should be less than 5mb!") 
-    else:
-        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @JEBotZ**',
-            disable_web_page_preview=True,
-        )
-    finally:
-        os.remove(download_location)
-
-@Client.on_message(filters.video)
-async def telegraphvid(client, message):
-    msg = await message.reply_text("Uploading To Telegraph...")
-    download_location = await client.download_media(
-        message=message, file_name='root/jetg')
-    try:
-        response = upload_file(download_location)
-    except:
-        await msg.edit_text("Video size should be less than 5mb!") 
-    else:
-        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @JEBotZ**',
-            disable_web_page_preview=True,
-        )
-    finally:
-        os.remove(download_location)
-
-@Client.on_message(filters.animation)
-async def telegraphgif(client, message):
-    msg = await message.reply_text("Uploading To Telegraph...")
-    download_location = await client.download_media(
-        message=message, file_name='root/jetg')
-    try:
-        response = upload_file(download_location)
-    except:
-        await msg.edit_text("Gif size should be less than 5mb!") 
-    else:
-        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @JEBotZ**',
-            disable_web_page_preview=True,
-        )
-    finally:
-        os.remove(download_location)
 
 @Client.on_callback_query()
 async def button(bot, update):
