2762e2a3538ff981efb405e84230e6459a907122
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-24 13:50:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 HELP_TEXT ="""ONUMILLLA PARA"""
 START_TEXT = """HLO SUGAMNO NDHDHBDBDJDB"""
 Peaky = Client(
-   "Telegraph Uploader",
+   "Test bot",
    api_id=Config.APP_ID,
    api_hash=Config.API_HASH,
    bot_token=Config.TG_BOT_TOKEN,
@@ -50,4 +50,67 @@ async def start(bot, update):
         parse_mode="html",
         reply_to_message_id=update.message_id
     )
+
+#now set call back 
+
+@Client.on_callback_query(filters.regex(r"^(start|help|about|close)$"), group=2)
+async def callback_data(bot, update: CallbackQuery):
+
+    query_data = update.data
+
+    if query_data == "start":
+        buttons = [[
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
+            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Filter-Bot-V2')
+        ],[
+            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        ],[
+            InlineKeyboardButton('Help ⚙', callback_data="help")
+        ]]
+    
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await update.message.edit_text(
+            Translation.START_TEXT.format(update.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode="html",
+            disable_web_page_preview=True
+        )
+
+
+    elif query_data == "help":
+        buttons = [[
+            InlineKeyboardButton('Home ⚡', callback_data='start'),
+            InlineKeyboardButton('About 🚩', callback_data='about')
+        ],[
+            InlineKeyboardButton('Close 🔐', callback_data='close')
+        ]]
+    
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await update.message.edit_text(
+            Translation.HELP_TEXT,
+            reply_markup=reply_markup,
+            parse_mode="html",
+            disable_web_page_preview=True
+        )
+
+
+    elif query_data == "about": 
+        buttons = [[
+            InlineKeyboardButton('Home ⚡', callback_data='start'),
+            InlineKeyboardButton('Close 🔐', callback_data='close')
+        ]]
+        
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await update.message.edit_text(
+            Translation.ABOUT_TEXT,
+            reply_markup=reply_markup,
+            parse_mode="html"
+        )
+
+
+    elif query_data == "close":
+        await update.message.delete()
 Peaky.run()
