80f21d693def426bf27ca53593a515f4e3d23f81
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-24 13:04:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,5 +12,23 @@ Peaky = Client(
    bot_token=Config.TG_BOT_TOKEN,
 )
 
+@Peakyt.on_message(filters.command(["help"]) & filters.private, group=1)
+async def help(bot, update):
+    buttons = [[
+        InlineKeyboardButton('Home ⚡', callback_data='start'),
+        InlineKeyboardButton('About 🚩', callback_data='about')
+    ],[
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+    ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_TEXT,
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
 
 Peaky.run()
