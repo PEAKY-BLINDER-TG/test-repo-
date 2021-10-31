1541c3c1a444c68d8a8b9c86402342c7793c8895
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-31 16:28:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,9 +31,7 @@ async def help(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=HELP_TEXT,
-        reply_markup=reply_markup,
         parse_mode="html",
-        reply_to_message_id=update.message_id
     )
 START_TEXT = "HEY BRUHH HOW ARE YOUUU"
 
@@ -44,6 +42,5 @@ async def wasim(bot, message):
     await bot.send_message(
         text=START_TEXT,
         parse_mode="html",
-        reply_to_message_id=update.message_id
     )
 Peaky.run()
