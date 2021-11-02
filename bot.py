f096aa0c5e5be93714431075f223a89f3cc127a0
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-11-02 19:04:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,4 +15,15 @@ Bot = Client(
    bot_token=TG_BOT_TOKEN,
 )
 
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    text = START_TEXT.format(update.from_user.mention)
+    reply_markup = START_BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=reply_markup
+    )
+
 Bot.run()
